af56fb9096ce751ed02380018649e5527f27e9a2
novadeinstar <novadeinstar@gmail.com>
2026-05-13 21:20:56 +0200

    three

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -196,6 +196,216 @@ export default function Team() {
           </motion.div>
         </motion.div>
 
+        {/* Emil */}
+        <motion.div
+          onHoverStart={() => setHovered(true)}
+          onHoverEnd={() => setHovered(false)}
+          initial={{
+            opacity: 0,
+            y: 80,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.8,
+            ease: "easeOut",
+          }}
+          whileHover={{
+            y: -10,
+            scale: 1.02,
+          }}
+        >
+          {/* IMAGE */}
+          <Image
+            src="/images/team/Emil.jpeg"
+            alt="David Shi"
+            width={433.33}
+            height={650}
+            style={{
+              width: "100%",
+              height: "500px",
+              objectFit: "cover",
+              borderRadius: "30px",
+            }}
+          />
+
+          {/* INFO BOX */}
+          <motion.div
+            whileHover={{
+              scale: 1.05,
+              y:-8,
+              paddingBottom: "40px",
+              boxShadow:
+                "0px 25px 60px rgba(59,130,246,0.25)",
+            }}
+            transition={{
+              type: "spring",
+              stiffness: 250,
+            }}
+            style={{
+              position: "relative",
+              bottom: "-30px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              width: "85%",
+              padding: "24px",
+              borderRadius: "24px",
+              background:
+                "rgba(255,255,255,0.08)",
+              backdropFilter: "blur(14px)",
+              border:
+                "1px solid rgba(255,255,255,0.08)",
+              boxShadow:
+                "0px 20px 50px rgba(0,0,0,0.35)",
+            }}
+          >
+            <h2
+              style={{
+                fontSize: "1.4rem",
+                marginBottom: "8px",
+              }}
+            >
+              Emil Raba
+            </h2>
+
+            <p
+              style={{
+                opacity: 0.75,
+                marginBottom: "12px",
+              }}
+            >
+              Co-Founder 
+              AI-Engineering
+            </p>
+
+            <motion.p
+                animate={{
+                  opacity: hovered ? 1 : 0,
+                  maxHeight: hovered ? 200 : 0,
+                }}
+              transition={{
+                duration: 0.3,
+              }}
+              style={{
+                overflow: "hidden",
+                marginTop: "14px",
+                fontSize: "0.92rem",
+                lineHeight: "1.6",
+              }}
+            >
+              Specialized in AI development.
+            </motion.p>
+          </motion.div>
+        </motion.div>
+
+        {/* Aarush */}
+        <motion.div
+          onHoverStart={() => setHovered(true)}
+          onHoverEnd={() => setHovered(false)}
+          initial={{
+            opacity: 0,
+            y: 80,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          viewport={{ once: true }}
+          transition={{
+            duration: 0.8,
+            ease: "easeOut",
+          }}
+          whileHover={{
+            y: -10,
+            scale: 1.02,
+          }}
+        >
+          {/* IMAGE */}
+          <Image
+            src="/images/team/Aarush.jpeg"
+            alt="David Shi"
+            width={433.33}
+            height={650}
+            style={{
+              width: "100%",
+              height: "500px",
+              objectFit: "cover",
+              borderRadius: "30px",
+            }}
+          />
+
+          {/* INFO BOX */}
+          <motion.div
+            whileHover={{
+              scale: 1.05,
+              y:-8,
+              paddingBottom: "40px",
+              boxShadow:
+                "0px 25px 60px rgba(59,130,246,0.25)",
+            }}
+            transition={{
+              type: "spring",
+              stiffness: 250,
+            }}
+            style={{
+              position: "relative",
+              bottom: "-30px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              width: "85%",
+              padding: "24px",
+              borderRadius: "24px",
+              background:
+                "rgba(255,255,255,0.08)",
+              backdropFilter: "blur(14px)",
+              border:
+                "1px solid rgba(255,255,255,0.08)",
+              boxShadow:
+                "0px 20px 50px rgba(0,0,0,0.35)",
+            }}
+          >
+            <h2
+              style={{
+                fontSize: "1.4rem",
+                marginBottom: "8px",
+              }}
+            >
+              Aarush Mayya
+            </h2>
+
+            <p
+              style={{
+                opacity: 0.75,
+                marginBottom: "12px",
+              }}
+            >
+              Co-Founder 
+              Software & APP
+            </p>
+
+            <motion.p
+                animate={{
+                  opacity: hovered ? 1 : 0,
+                  maxHeight: hovered ? 200 : 0,
+                }}
+              transition={{
+                duration: 0.3,
+              }}
+              style={{
+                overflow: "hidden",
+                marginTop: "14px",
+                fontSize: "0.92rem",
+                lineHeight: "1.6",
+              }}
+            >
+              Specialized in Softwaresystem.
+            </motion.p>
+          </motion.div>
+        </motion.div>
+
       </section>
     </main>
   );
